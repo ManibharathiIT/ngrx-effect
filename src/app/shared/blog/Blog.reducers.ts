d2a57a7bf05ed391c45df122aa1d42574e8a7bcb
ManibharathiIT <manibharathi5648@gmail.com>
2024-09-24 17:57:28 +0530

    commands added

--- src/app/shared/blog/Blog.reducers.ts
+++ src/app/shared/blog/Blog.reducers.ts
@@ -8,11 +8,13 @@ export const _blogReducer = createReducer(BlogState,
             ...state
         };
     }),
-    on(loadblogsuccess, (state, action) => {
+    on(loadblogsuccess, (state, action) => {  //The on function is being used to listen for the loadBlogSuccess action. When this action is dispatched, the provided callback (state, action) is executed.
+        //state represents the current state of the slice of the application, and action contains any data passed with the loadBlogSuccess action.
         return {
-            ...state,
-            bloglist: [...state.bloglist],
-            Errormessage: ''
+            ...state,//This spreads the current state object into a new object. It ensures that all existing properties of the state remain unchanged unless explicitly overridden in the following lines.
+            bloglist: [...state.bloglist],//This creates a shallow copy of the bloglist array from the current state.
+                                        //It does not seem to be modifying the contents of bloglist here; instead, it's just copying the array. This is likely done to maintain immutability, which is a core principle of Redux. Even though no changes are made to bloglist, creating a new reference ensures that the state update is handled properly in case other parts of the application depend on detecting changes.
+            Errormessage: ''  //This typically happens when a previous attempt to load blogs might have failed, but since this is a success action (loadBlogSuccess), the error message is cleared.
         }
     }),
     on(loadblogfail, (state, action) => {
